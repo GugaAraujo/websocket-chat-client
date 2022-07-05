3e377538c07b20abbe92d126183674685ed0da98
Gustavo Araújo <gustavo.avk@msn.com>
2022-07-04 23:52:26 -0300

    feat(Chat): CSS Loader while connecting

--- components/Chat.vue
+++ components/Chat.vue
@@ -1,7 +1,13 @@
 <template>
-  <div ref="chatbox" class="chatbox m-auto my-4 p-4 shadow-lg w-4/5 bg-white bg-opacity-20 backdrop-blur-xl rounded drop-shadow-lg overflow-y-scroll">
-    <div v-for="message in allMessages" :key="message">
-      <Message :message="message" />
+  <div ref="chatbox"
+    class="chatbox m-auto my-4 p-4 shadow-lg w-4/5 bg-white bg-opacity-20 backdrop-blur-xl rounded drop-shadow-lg overflow-y-scroll">
+    <div v-if="allMessages.length">
+      <div v-for="message in allMessages" :key="message">
+        <Message :message="message" />
+      </div>
+    </div>
+    <div v-else class="m-auto w-full text-center">
+      <DotsLoader />
     </div>
   </div>
 </template>
@@ -9,12 +15,14 @@
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api'
 import Message from '@/components/Message.vue'
+import DotsLoader from './DotsLoader.vue'
 
 export default defineComponent({
   name: 'Chat',
   components: {
-    Message
-  },
+    Message,
+    DotsLoader
+},
   props: {
     allMessages: {
       type: Array,
@@ -23,7 +31,7 @@ export default defineComponent({
   },
   watch: {
     allMessages: {
-      async handler () {
+      async handler() {
         const chatbox = this.$refs.chatbox as HTMLDivElement
         await this.$nextTick()
         chatbox.scrollTop = chatbox.scrollHeight
@@ -39,14 +47,21 @@ export default defineComponent({
   /* Foreground, Background */
   scrollbar-color: #4291D4 rgb(173, 233, 235);
 }
+
 .chatbox::-webkit-scrollbar {
-  width: 15px; /* Mostly for vertical scrollbars */
-  height: 15px; /* Mostly for horizontal scrollbars */
+  width: 15px;
+  /* Mostly for vertical scrollbars */
+  height: 15px;
+  /* Mostly for horizontal scrollbars */
 }
-.chatbox::-webkit-scrollbar-thumb { /* Foreground */
+
+.chatbox::-webkit-scrollbar-thumb {
+  /* Foreground */
   background: #4291D4;
 }
-.chatbox::-webkit-scrollbar-track { /* Background */
+
+.chatbox::-webkit-scrollbar-track {
+  /* Background */
   background: rgb(173, 233, 235);
 }
 </style>
