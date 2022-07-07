1f77b4f7efcbd7a4c9753190966363fde572d5aa
Gustavo Araújo <gustavo.avk@msn.com>
2022-07-07 11:54:41 -0300

    feature(server): Providing access via local IP

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,8 @@
 export default {
+  server: {     
+    port: 3000, // default: 3000     
+    host: '0.0.0.0', // default: localhost   
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Websocket Chat Project',
